6f4bda589cb9af5048af70bbd89a93a3f63f17a3
Justin Phillips <justinphil02@yahoo.com>
2019-10-13 23:37:15 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,11 @@
 def first_challenge
-   epic_tragedy = {}
+  
+  #hash level 1 - epic tragedy 
+  
+   epic_tragedy = {
+     #hash level 2 - family names 
+    :montague => {},
+    :capulet => {} 
+      }
+   
 end
